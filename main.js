3a072ad724b37c29365ca772fc6c6af646007249
nmaitchison <nmaitchison@gmail.com>
2017-06-15 15:33:54 -0700

    fixed js to load after html

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -14,17 +13,24 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slide");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
+
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
+
   dots[slideIndex-1].className += " active";
 }
+
 /*
 var slideIndex = 0;
 showSlides();
@@ -32,12 +38,17 @@ showSlides();
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1}
+    if (slideIndex> slides.length) {
+      slideIndex = 1
+    }
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
+
+
 }
 */
