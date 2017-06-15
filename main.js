a00daf2528b6b1e573c72e31ffa0a0392a57ca64
nmaitchison <nmaitchison@gmail.com>
2017-06-14 19:43:30 -0700

    fixing

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+/var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -9,15 +9,23 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slide");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
+
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
