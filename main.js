e03d68b2ee719825fa9f899d63c88f114dfe89ae
nmaitchison <nmaitchison@gmail.com>
2017-06-15 16:15:52 -0700

    setTimeout to 6 secs

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
 
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
+    setTimeout(showSlides, 6000); // Change image every 4 seconds
 }
 
 
